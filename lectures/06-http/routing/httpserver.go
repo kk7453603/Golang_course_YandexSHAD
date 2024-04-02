cb1bdef5ff08df92e457ac3cedb9f0ebfa3f3ded
pkositsyn <kositsyn.pa@phystech.edu>
2024-04-02 23:40:59 +0300

    httpserver with routing

--- lectures/06-http/routing/httpserver.go
+++ lectures/06-http/routing/httpserver.go
@@ -8,9 +8,16 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("GET /posts/{id}", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println(r.PathValue("id"))
-	})
+
+	mw := func(h http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			fmt.Println(r.PathValue("id"))
+			h.ServeHTTP(w, r)
+		})
+	}
+	mux.Handle("GET /posts/{id}", mw(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("yes")
+	})))
 	r := httptest.NewRecorder()
 	mux.ServeHTTP(r, httptest.NewRequest(http.MethodGet, "/posts/123", nil))
 }
